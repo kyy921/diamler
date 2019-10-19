43c448464b99cf0b93df85d4cc634e2355696579
kang_yuying <kyy921@gmail.com>
2019-10-20 04:35:04 +0800

    package all

---
+++ build/precache-manifest.0e6780f47dd3e8a42dfb0e90c8bd2b90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "77ea7407f623677f7a87",
+    "url": "./static/js/main.b8a497b6.chunk.js"
+  },
+  {
+    "revision": "7d9cd342ee4b726aac11",
+    "url": "./static/js/2.7ea55338.chunk.js"
+  },
+  {
+    "revision": "77ea7407f623677f7a87",
+    "url": "./static/css/main.9960f11e.chunk.css"
+  },
+  {
+    "revision": "7d9cd342ee4b726aac11",
+    "url": "./static/css/2.b960dadd.chunk.css"
+  },
+  {
+    "revision": "444db68cb7335dbbf0abb79362eac0fb",
+    "url": "./index.html"
+  }
+];
